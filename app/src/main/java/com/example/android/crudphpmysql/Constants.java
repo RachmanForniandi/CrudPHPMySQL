f1b32d500dc56a22210cabb4845d2a78da98670f
RachmanForniandi <rachmanforniandi@gmail.com>
2017-07-20 10:38:58 +0700

    buat UI register utk user

---
+++ app/src/main/java/com/example/android/crudphpmysql/Constants.java
@@ -0,0 +1,12 @@
+package com.example.android.crudphpmysql;
+
+/**
+ * Created by Lenovo on 7/20/2017.
+ */
+
+public class Constants {
+
+    public static final String ROOT_URL = "http://192.168.1.9/crud_android/v1/";
+    public static final String URL_REGISTER = ROOT_URL+"registerUser.php";
+
+}
