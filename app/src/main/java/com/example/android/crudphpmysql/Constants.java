0a0fab87424da95c500805ac4bb8e0031dd99190
RachmanForniandi <rachmanforniandi@gmail.com>
2017-07-24 02:08:13 +0700

    membuat sistem login user

--- app/src/main/java/com/example/android/crudphpmysql/Constants.java
+++ app/src/main/java/com/example/android/crudphpmysql/Constants.java
@@ -8,5 +8,6 @@ public class Constants {
 
     public static final String ROOT_URL = "http://192.168.1.13/crud_android/v1/";
     public static final String URL_REGISTER = ROOT_URL+"registerUser.php";
+    public static final String URL_LOGIN = ROOT_URL+"userLogin.php";
 
 }
